b9b4d9b81e5265990acdfd4b8c8a61b8e9ff7894
Brad Lucas <brad@beaconhill.com>
2017-01-20 21:40:47 -0500

    remove call from command line

--- src/hackerrank/core.clj
+++ src/hackerrank/core.clj
@@ -1,9 +1,10 @@
 (ns hackerrank.core
   (:use [clojure.string :only (split triml)])
-  (:require [hackerrank.algorithms.warmup :refer :all]
-            [hackerrank.functional.introduction.introduction :refer :all]
-            [hackerrank.functional.recursion.recursion :refer :all])
+  ;; (:require [hackerrank.algorithms.warmup :refer :all]
+  ;;           [hackerrank.functional.introduction.introduction :refer :all]
+  ;;           [hackerrank.functional.recursion.recursion :refer :all])
   (:gen-class))
 
 (defn -main [& args]
-  (run-eval-ex))
+  ;;(run-eval-ex)
+  )
