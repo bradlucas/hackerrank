e1ab336a1f5db996e58e2046c8fb54e5413d4acf
Brad Lucas <brad@beaconhill.com>
2017-01-27 08:14:20 -0500

    testing even-tree

--- src/hackerrank/core.clj
+++ src/hackerrank/core.clj
@@ -3,8 +3,8 @@
   ;; (:require [hackerrank.algorithms.warmup :refer :all]
   ;;           [hackerrank.functional.introduction.introduction :refer :all]
   ;;           [hackerrank.functional.recursion.recursion :refer :all])
+  (:require [hackerrank.algorithms.even-tree :as g])
   (:gen-class))
 
 (defn -main [& args]
-  ;;(run-eval-ex)
-  )
+  (g/run))
