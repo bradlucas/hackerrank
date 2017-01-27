a624950a98d1b05dae86254954e30fd0bf39732a
Brad Lucas <brad@beaconhill.com>
2017-01-27 08:57:02 -0500

    passing test case

--- src/hackerrank/algorithms/even_tree.clj
+++ src/hackerrank/algorithms/even_tree.clj
@@ -1,7 +1,5 @@
 (ns hackerrank.algorithms.even-tree)
 
-
-
 ;; Input
 ;; 10 9            - N number of vertices, M number of edges
 ;; 2 1             - Edges
@@ -27,16 +25,45 @@
 
 
 
+(defn build-edge [[a b]] 
+  (let [rtn (if (not= a b)
+              [;;{:source a :dest b}
+               {:source b :dest a}
+               ])]
+    rtn))
+
+(defn build-graph
+  [edges]
+  (loop [edges edges
+         rtn #{}]
+    (if (empty? edges)
+      rtn
+      (recur (rest edges) (apply conj rtn (build-edge (first edges)))))))
+
+(defn get-children [g n] 
+  (let [get-node (fn [b n] (filter (fn [m] (= n (:source m))) g))]
+    (map :dest (get-node g n))))
+
+(defn queue
+      ([] clojure.lang.PersistentQueue/EMPTY)
+      ([coll] (reduce conj clojure.lang.PersistentQueue/EMPTY coll)))
+
+
+(defn node-count [graph node]
+  (let [children (get-children graph node)]
+    (if (empty? children)
+      1
+      (apply + 1 (map (fn [n] (node-count graph n)) children)))))
 
 
 (defn process
   [num-nodes edges]
-  (println num-nodes)
-  (println edges))
-
-
-
-
+  ;; get the node-counts for the children of 1
+  (let [graph (build-graph edges)
+        f (partial node-count graph)
+        node-counts (map f (get-children graph 1))]
+    ;; return the number of even counts
+    (count (filter even? node-counts))))
 
 
 (defn get-pair [] 
@@ -60,6 +87,13 @@
   []
   (let [[num-nodes num-edges] (get-pair)]
     (let [edges (read-edges num-edges)]
-      (process num-nodes edges))))
+      (println (process num-nodes edges)))))
 
 ;; (run)
+
+
+
+;; TESTING
+;; (def g (build-graph [[2 1] [3 1] [4 3] [5 2] [6 1] [7 2] [8 6] [9 8] [10 8]]))
+;;
+;; (process 10 [[2 1] [3 1] [4 3] [5 2] [6 1] [7 2] [8 6] [9 8] [10 8]])
