f037e424a52a519b936303a3f25ff69eb2a4bb51
Brad Lucas <brad@beaconhill.com>
2017-11-01 21:40:00 -0400

    jumping bunnies

---
+++ src/hackerrank/functional/adhoc/jumping_bunnies.clj
@@ -0,0 +1,23 @@
+(ns hackerrank.functional.adhoc.jumping-bunnies)
+
+
+
+(defn gcd 
+      [a b]
+      (if (zero? b)
+      a
+      (recur b, (mod a b))))
+ 
+(defn lcm 
+      [a b]
+      (/ (* a b) (gcd a b)))
+
+
+
+(defn run []
+  (let [num (Integer/parseInt (read-line))
+       num-list (mapv #(Integer/parseInt %) (clojure.string/split (clojure.string/trim (read-line)) #"\s+"))]
+    (println (reduce lcm num-list))))
+
+;; (run)
+
